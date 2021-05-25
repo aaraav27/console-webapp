cf8cf0ea6a2f6bb1d003f83e01609170e809b025
aaraav27 <74003571+aaraav27@users.noreply.github.com>
2021-05-25 11:23:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("my first webpage");
+
+var firstname = "aarav";
+var lastname =  "khandelwal";
+
+var p = firstname.concat(lastname);
+console.log(p);
+
+var num1 = 50;
+var num2 = 30;
+console.log(50+30);
+
+let sqnum = Math.sqrt(16);
+console.log(sqnum);
+
+let maxnum = Math.max(4003 , 4003, 4003);
+console.log(maxnum);
